a2f0467442f4342bd8fb93dde44c709bff76c13c
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-07-25 20:48:54 +0530

    Update ansibleRoleCD.groovy

--- vars/ansibleRoleCD.groovy
+++ vars/ansibleRoleCD.groovy
@@ -1,8 +1,27 @@
-def call(String playbook, String creds, String branch) {
-    checkout(playbook, creds, branch)
-    ansibleLint()
-    dryRun(playbook)
-    gitleaks()
-    credScan()
-    syntaxCheck(playbook)
+def checkout(String url, String creds, String branch) {
+    org.ansible.Checkout(url, creds, branch)
+}
+
+def gitleaks() {
+    org.ansible.Gitleaks()
+}
+
+def credScan() {
+    org.ansible.CredScan()
+}
+
+def ansibleLint(String playbook) {
+    org.ansible.AnsibleLint(playbook)
+}
+
+def syntaxCheck(String playbook) {
+    org.ansible.SyntaxCheck(playbook)
+}
+
+def dryRun(String playbook) {
+    org.ansible.DryRun(playbook)
+}
+
+def deploy(String playbook) {
+    org.ansible.Deploy(playbook)
 }
