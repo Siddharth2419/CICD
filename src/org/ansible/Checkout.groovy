40732193ce5d69b76241e348f19458da155c2962
Siddharth2419 <160397748+Siddharth2419@users.noreply.github.com>
2024-07-25 20:19:06 +0530

    Update Checkout.groovy

--- src/org/ansible/Checkout.groovy
+++ src/org/ansible/Checkout.groovy
@@ -1,1 +1,9 @@
+package org.ansible
+
+def call(String url, String creds, String branch) {
+    checkout([$class: 'GitSCM',
+        branches: [[name: branch]],
+        userRemoteConfigs: [[url: url, credentialsId: creds]]
+    ])
+}
 
